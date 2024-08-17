dd775a6719799844524843db77f78b233cf8115d
Saatvik Tammishetty <saatvik.tammishetty@gmail.com>
2024-08-17 19:56:46 +0530

    Added Transitions

--- Turing.cpp
+++ Turing.cpp
@@ -7,6 +7,7 @@ private:
     set<string> states;
     string initial_state;
     set<char> tape_symbols;
+    map<tuple<string, char>, tuple<string, char, char>> transitions;
 
 public:
     void get_TM_specs_from_user() {
@@ -32,6 +33,17 @@ public:
 
         cout << "Enter initial state: ";
         cin >> initial_state;
+
+        int n_transitions;
+        cout << "Enter number of transitions: ";
+        cin >> n_transitions;
+        for (int i = 0; i < n_transitions; i++) {
+            string state, next_state;
+            char read_symbol, write_symbol, action;
+            cout << "Enter transition (state, read_symbol, next_state, write_symbol, action): ";
+            cin >> state >> read_symbol >> next_state >> write_symbol >> action;
+            transitions[make_tuple(state, read_symbol)] = make_tuple(next_state, write_symbol, action);
+        }
     }
 
     void print_TM_specs() {
@@ -48,6 +60,16 @@ public:
         cout << "}\n";
 
         cout << "Initial state: " << initial_state << endl;
+
+        cout << "Transitions: " << endl;
+        for (const auto &transition : transitions) {
+            string state = get<0>(transition.first);
+            char read_symbol = get<1>(transition.first);
+            string next_state = get<0>(transition.second);
+            char write_symbol = get<1>(transition.second);
+            char action = get<2>(transition.second);
+            cout << "(" << state << ", " << read_symbol << ") -> (" << next_state << ", " << write_symbol << ", " << action << ")\n";
+        }
     }
 };
 
